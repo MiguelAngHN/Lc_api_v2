90ad7be86fa58e171499dc7092638fe05ad61de0
tu_nombre <tu_correo>
2024-04-09 22:13:36 -0500

    Agregando middleware para api movil

--- app/Models/User.php
+++ app/Models/User.php
@@ -115,4 +115,25 @@ public function scopeSort(Builder $query){
    public function actividades(){
       return $this->hasMany(Actividad::class, 'user_id');
    }
+
+          /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        // 'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
 }
